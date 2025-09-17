ab243dc38b4dad49552e2ea6d4c49bf1de7bb031
alexlancaster <alexlancaster@users.noreply.github.com>
2025-09-17 06:23:21 +0000

    Deploy Amber Research site preview [skip ci]

---
+++ assets/js/fusejs/fusejs-store.js
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2013-2020 Michael Rose and contributors
+MIT License
+From minimal-mistakes: https://github.com/mmistakes/minimal-mistakes/
+*/
+var store = [{
+        "title": "Archive",
+        "subtitle": "",
+        "excerpt": "",
+        "content": "",
+        "categories": null,
+        "date": null,
+        "tags": null,
+        "url": "/archive",
+        "img": null
+      }]
